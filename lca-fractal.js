1b4dc2e5e401ecaf66b68c4ed36506153cdf8892
Tim Peter <tim.peter@eonum.ch>
2012-11-25 17:22:33 +0100

    cliping region

--- lca-fractal.js
+++ lca-fractal.js
@@ -1,25 +1,30 @@
 var canvas = document.getElementById('artmachine');
 var context = canvas.getContext('2d');
 
+var x = canvas.width / 2;
+var y = canvas.height / 2;
+
+context.save();
+context.beginPath();
+context.arc(x, y, 402, 0, 2 * Math.PI, false);
+context.clip();
+
+
 context.lineWidth = 5;
 context.strokeStyle = 'blue';
 context.beginPath();
 context.moveTo(100, 150);
 context.lineTo(450, 50);
-//context.stroke();
+context.stroke();
 
+var startAngle = 0.0 * Math.PI;
+var endAngle = 2.0 * Math.PI;
+var counterClockwise = false;
 
-      var x = canvas.width / 2;
-      var y = canvas.height / 2;
-      var radius = 400;
-      var startAngle = 0.0 * Math.PI;
-      var endAngle = 2.0 * Math.PI;
-      var counterClockwise = false;
+context.beginPath();
+context.arc(x, y, 400, startAngle, endAngle, counterClockwise);
+context.lineWidth = 3;
 
-      context.beginPath();
-      context.arc(x, y, radius, startAngle, endAngle, counterClockwise);
-      context.lineWidth = 3;
-
-      // line color
-      context.strokeStyle = 'black';
-      context.stroke();
+// line color
+context.strokeStyle = 'black';
+context.stroke();
